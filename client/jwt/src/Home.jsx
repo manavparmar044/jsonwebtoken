e696a5a6d99b10f262e42b69a92ebf0d7c3b5099
Manav Manish Parmar <mmparmar044@gmail.com>
2024-08-24 22:46:46 +0530

    Home css updated

--- client/jwt/src/Home.jsx
+++ client/jwt/src/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import "./Home.css"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="home-container">
+      <div className="home-text">Home</div>
+    </div>
   )
 }
 
